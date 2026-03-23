a686995f4b701ddbfc26e6cbe2737ef6dba73c99
MicheleTodeschini <micheletodeschini01@gmail.com>
2026-03-23 19:38:09 +0100

    solved milestone 1

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,22 @@ import './App.css'
 
 function App() {
 
+  const products = [
+    { name: 'Mela', price: 0.5 },
+    { name: 'Pane', price: 1.2 },
+    { name: 'Latte', price: 1.0 },
+    { name: 'Pasta', price: 0.7 },
+  ];
 
   return (
     <>
+      <h1>La mia lista della spesa</h1>
 
+      <ul>
+        {products.map(product => (
+          <li>{product.name} e costa {product.price} € </li>
+        ))}
+      </ul>
     </>
   )
 }
